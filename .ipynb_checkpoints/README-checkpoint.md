8bbfe2484cc06d051828e70a5e79e1c1eb3440d3
adityakamath <adityakamath@live.com>
2021-04-04 19:05:38 +0200

    added loader command switcher

--- .ipynb_checkpoints/README-checkpoint.md
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -1,10 +1,10 @@
 # akros_jetson
 
 ### Description
-ROS launch files and utilities for the AKROS robots based on the Jetson Nano (Jetracer2, Jetbot2). This node also contains the high-level control script, that switches between different driving modes and publishes the Twist message that the robot needs to drive. The following functionalities are implemented
+ROS launch files and utilities for the AKROS robots based on the Jetson Nano (Jetracer2, Jetbot2). This node also contains the high-level control script, that switches between different driving modes and publishes the messages that the robot needs to drive. The following functionalities are implemented
 * control_mode_switcher.py
-  * Subscribes to the mode value from the joystick, the twist message from the akros_joystick package and a twist message from another node providing autonomous functionality
-  * Based on the mode, the output twist message is either the teleop twist command from akros_joystick, the twist message from the autonomous node or a combination of both (semi-autonomous: linear velocity via teleop, angular via autonomous node)
+  * Subscribes to the mode value from the joystick, the twist message from the akros_joystick package and a twist message from another node providing autonomous functionality. Similarly, for the loader attachment, this node subscribes to the loader up/down value from the joystick, from the autonomous mode and based on the mode value, a switched output is provided.
+  * Based on the mode, the output twist message is either the teleop twist command from akros_joystick, the twist message from the autonomous node or a combination of both (semi-autonomous: linear velocity via teleop, angular via autonomous node). Similarly, for the loader, the output is either the up/down commands from the joystick (for the teleop and semi-autonomous mode) or from the auto mode for mode 3.
 * Launch files
   * The 'launch' directory contains the launch files to drive the AKROS Ackermann and Differential platforms
   * Each launch files launches all the nodes required to drive the particular robot
@@ -17,12 +17,15 @@ ROS launch files and utilities for the AKROS robots based on the Jetson Nano (Je
 
 ### Subscribers
 The node subscribes to the following topics:
-* `/mode`: Selected driving mode from the joystick package
-* `/teleop/cmd_vel`: Twist message received from the joystick package
-* `/auto/cmd_vel`: Twist message received from the autonomy package 
+* `/mode`: Selected driving mode from the joystick node
+* `/teleop/cmd_vel`: Twist message received from the joystick node
+* `/auto/cmd_vel`: Twist message received from the autonomy node 
+* `/teleop/loader`: Loader up/down message (Int8) received from the autonomy node
+* `/auto/loader`: Loader up/down message (Int8) received from the autonomy node
+
 
 ### Publishers
-The node publishes the `/switch_node/cmd_vel` topic that is a combination of the subscribed twist messages and dependent on the selected mode. 
+The node publishes the `/switch_node/cmd_vel` topic that is a combination of the subscribed twist messages and dependent on the selected mode. For the loader attachment, this node also publishes the `/switch_node/loader` topic that is either the teleop loader message or the auto message based on the mode.
 
 ### Launch
 The following launch files provided:
--- scripts/.ipynb_checkpoints/control_mode_switcher-checkpoint.py
+++ scripts/.ipynb_checkpoints/control_mode_switcher-checkpoint.py
@@ -7,8 +7,9 @@ This code implements the following functionality:
   > Subscribes to twist messages from teleop node
   > Subscribes to twist messages from auto node
   > Subscribes to integer messages from the mode selection node
-  > Switches output twist messages according to selected mode
-  > Publishes output twist message
+  > Subscribes to integer messages from loader up/down selection
+  > Switches output twist messages according to selected mode and sets correct up/down messages for loader
+  > Publishes output twist message and output loader up/down message
 by Aditya Kamath
 adityakamath.github.io
 github.com/adityakamath
@@ -28,6 +29,14 @@ class ModeSwitcher():
         self.ros_sub_teleop = rospy.Subscriber("/mode", Int8, self.set_mode, queue_size=1)
         rospy.loginfo("[MSW] Teleop Twist Subscriber initialized")
         
+        #--- Create the Subscriber to read Teleop loader up/down selection
+        self.ros_sub_loader = rospy.Subscriber("/teleop/loader", Int8, self.set_teleop_loader, queue_size=1)
+        rospy.loginfo("[MSW] Teleop Loader Up/Down Subscriber initialized")
+        
+        #--- Create the Subscriber to read Auto loader up/down selection
+        self.ros_sub_loader = rospy.Subscriber("/auto/loader", Int8, self.set_auto_loader, queue_size=1)
+        rospy.loginfo("[MSW] Auto Loader Up/Down Subscriber initialized")
+        
         #--- Create the Subscriber to Teleop Twist commands
         self.ros_sub_teleop = rospy.Subscriber("/teleop/cmd_vel", Twist, self.set_teleop_twist, queue_size=1)
         rospy.loginfo("[MSW] Teleop Twist Subscriber initialized")
@@ -36,15 +45,21 @@ class ModeSwitcher():
         self.ros_sub_auto = rospy.Subscriber("/auto/cmd_vel", Twist, self.set_auto_twist, queue_size=1)
         rospy.loginfo("[MSW] Auto Twist Subscriber initialized")
         
-        # initialize twist variables for teleop/cmd_vel and auto/cmd_vel, and integer variable for mode
-        self.teleop_msg = Twist()
-        self.auto_msg   = Twist()
-        self.mode       = 0
-        self.prev_mode  = 0
+        # initialize twist variables for teleop/cmd_vel, auto/cmd_vel, teleop/loader, auto/loader, and integer variable for mode
+        self.teleop_msg    = Twist()
+        self.auto_msg      = Twist()
+        self.mode          = 0
+        self.prev_mode     = 0
+        self.teleop_loader = 0
+        self.auto_loader   = 0
 
         #--- Create the Publisher to send switched Twist commands
         self.ros_pub_switched = rospy.Publisher("/switch_node/cmd_vel", Twist, queue_size=1)
         rospy.loginfo("[MSW] Switched Twist Publisher initialized")
+        
+        #--- Create the Publisher to send switched Loader commands
+        self.ros_pub_switched_loader = rospy.Publisher("/switch_node/loader", Int8, queue_size=1)
+        rospy.loginfo("[MSW] Switched Loader Up/Down Publisher initialized")
 
         rospy.loginfo("[MSW] Initialization complete")
         
@@ -53,6 +68,12 @@ class ModeSwitcher():
         
     def set_auto_twist(self, msg):
         self.auto_msg = msg
+        
+    def set_teleop_loader(self, msg):
+        self.teleop_loader = msg
+        
+    def set_auto_loader(self, msg):
+        self.auto_loader = msg
 
     def set_mode(self, msg):
         self.mode = msg.data
@@ -72,23 +93,27 @@ class ModeSwitcher():
             if self.mode == 0:
                 mode0 = self.teleop_msg
                 mode0.linear.x = self.teleop_msg.linear.x * 0.75
-                mode0.angular.z = self.teleop_msg.angular.z* 0.75
+                mode0.angular.z = self.teleop_msg.angular.z * 0.75
                 self.ros_pub_switched.publish(mode0)
+                self.ros_pub_switched_loader.publish(0)
             
             # manual control
             elif self.mode == 1:
                 #rospy.loginfo("[MSW] Mode:1 Teleop")
                 self.ros_pub_switched.publish(self.teleop_msg)
+                self.ros_pub_switched_loader.publish(self.teleop_loader)
             
-            # autonomous steering, manual throttle
+            # autonomous steering, manual throttle, manual loader
             elif self.mode == 2:
                 mode2 = self.auto_msg
                 mode2.linear.x = self.teleop_msg.linear.x
                 self.ros_pub_switched.publish(mode2)
+                self.ros_pub_switched_loader.publish(self.teleop_loader)
             
             # fully autonomous
             elif self.mode == 3:
                 self.ros_pub_switched.publish(self.auto_msg)
+                self.ros_pub_switched_loader.publish(self.auto_loader)
             
             elif self.mode > 3:
                 rospy.logerr("[MSW] Invalid mode")
